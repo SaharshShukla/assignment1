445e7e9342be82edfabf6603944e0730cdbec8a9
SaharshShukla <89912999+SaharshShukla@users.noreply.github.com>
2021-11-09 15:23:42 +0530

    Add files via upload
    assignment 8nov class

---
+++ assignment1.java
@@ -0,0 +1,52 @@
+package assignments;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// qst1) print 1 to 10 in forward order via while loop
+	/*	int n=1;
+		while(n<=10) {
+			System.out.println(n);
+			n++;
+}*/
+		
+//qst2) print 10 to 1  in reversed order via while loop		
+	/*	int n=10;
+		while(n>=1) {
+			System.out.println(n);
+			n--;
+		}*/
+		
+//qst3) print 10 to 2  in reversed order via while loop		
+	
+	/*int n=10;
+	while(n>=2) {
+	System.out.println(n);
+	n=n-2;
+	}*/
+
+//qst4) print 2 to 10  in forward order via while loop
+	
+/*int j=2;
+while(j<=10) {
+	System.out.println(j);
+	j=j+2;
+}*/
+	
+		
+//qst5) print 2 to 10  in forward order via for loop
+		
+		/*for(int n=2;n<=10;n=n+2) {
+			System.out.println(n);
+		}*/
+		
+//qst6) print 10 to 2  in reversed order via for loop
+		for(int i=10;i>=2;i=i-2) {
+			System.out.println(i);
+		}
+	}
+	
+	
+
+}
