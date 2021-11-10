4d40e128dca3b5bcbdac2ec49618b2ca9002dc48
SaharshShukla <89912999+SaharshShukla@users.noreply.github.com>
2021-11-10 14:46:59 +0530

    Add files via upload

---
+++ assignments2.java
@@ -0,0 +1,34 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class assignments2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//printing a prime number from 2 to n=11
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter  first number: ");
+		int f=sc.nextInt();
+		System.out.print("Enter  second number: ");
+		int s=sc.nextInt();
+		for(int i=f;i<s;i++) {
+			if(isPrime(i)) {
+				System.out.println(i+" is prime");
+			}
+		}
+	}
+
+	static boolean isPrime(int n) {
+		if(n<=1) {
+			return(false);
+		}
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(n%i==0) {
+				return(false);
+			}
+			
+		}
+		return(true);
+	}
+}
