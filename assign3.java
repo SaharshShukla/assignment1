0f4505374a19e71fc8e320844007d667a17d1960
SaharshShukla <89912999+SaharshShukla@users.noreply.github.com>
2021-11-12 13:47:31 +0530

    assignment 3
    assignment 3

---
+++ assign3.java
@@ -0,0 +1,83 @@
+package assignments3;
+
+import java.util.Scanner;
+
+public class assign3 {
+
+	public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		// qst1) print sum of 1 to n taking user input and while loop
+		/*Scanner sc=new Scanner(System.in);
+			int a=1;
+			int sum=0; 
+			System.out.print("The till you want to print number: ");
+			int n=sc.nextInt();
+				while(a<=n) {
+					sum=a+sum;
+					System.out.println(sum);
+					a++;
+			}*/
+		
+		
+//qst2) factorial of number via user input and for loop
+		
+		      /*  int dis,num;
+				System.out.print("Enter number: ");
+				num=sc.nextInt();
+				dis=pass(num);
+				System.out.println("Factorial is: "+dis);
+			}
+			static int pass(int n) {
+				int f=1;
+				for(int i=n;i>=1;i--) {
+					f=f*i;
+				}
+				return(f);
+			}*/
+			
+			
+// qst3) printing table till n via user input and for loop
+				/*System.out.print("enter any number: ");
+				int nu=sc.nextInt();
+				for(int i=1;i<=10;i++) {
+					int pro=nu*i;
+					System.out.println(nu+"*"+i+"="+pro);
+				}*/
+		
+		
+//qst3) swaping of two number till n  via user input
+		/*Scanner sc=new Scanner(System.in);
+		System.out.print("Enter  first number: ");
+		int a=sc.nextInt();
+		System.out.print("Enter  second number: ");
+		int b=sc.nextInt();
+		System.out.println("Enter  the  n: ");
+		int n=sc.nextInt();
+		System.out.println("Before  swaping number a is : "+a+" and number b is : "+b);
+		for(int i=0;i<=n;i++) {
+			a=a-b;
+			b=a+b;
+			a=b-a;
+			System.out.println("After  swaping number a is :"+a+" and number b is : "+b);
+		}*/
+	
+		
+//qst 4) printing fibonacci series till n
+		/*Scanner sc=new Scanner(System.in);
+		int a=0;
+		int b=1;
+		System.out.print("Enter nth variable: ");
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++) {
+			System.out.println(a);
+			int c =a+b;
+			a=b;
+			b=c;
+		}*/
+		
+			}
+}
+
+	
+
+
