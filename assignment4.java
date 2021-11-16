175d33436c03fcc541fae5ef016f7bd58cf46bf2
SaharshShukla <89912999+SaharshShukla@users.noreply.github.com>
2021-11-16 10:35:00 +0530

    Update assignment4.java

--- assignment4.java
+++ assignment4.java
@@ -44,46 +44,46 @@ public class assignment4 {
         }*/
 		
 //qst3) reversed Pyramid with number pattern 2
-	 //not working
+	 
 		/*int n=5;
-		        for ( int i=n;i>=1;i-- ){     
+		        for ( int i=0;i<=n-1;i++){     
 
-		            for (int j=1;j<=(n-i);j++){ 
+		            for (int j=0;j<=i;j++){ 
 		                System.out.print(" ");
 		            }
 
-		            for (int j=1;j<=i;j++){
-		                System.out.print(i+" ");//just add j in place of *
+		            for (int k=0;j<=n-1-i;k++){
+		                System.out.print((i+1)+" ");//(i+1 for printing )
 		            }
 		            System.out.println();
 		        }*/
+	
 /* o/p
- 5 5 5 5 5 
- 4 4 4 4 
-  3 3 3 
-   2 2 
-    1 */
+ 1 1 1 1 1 
+  2 2 2 2 
+   3 3 3 
+    4 4 
+     5  */
 		
 		//qst4) reversed Pyramid with number pattern 1
-		//not working
 			/*int n=5;
-			        for ( int i=n;i>=1;i-- ){     
+			        for ( int i=1;i<=n;i++ ){     
 
-			            for (int j=1;j<=(n-i);j++){ 
+			            for (int j=1;j<=i;j++){ 
 			                System.out.print(" ");
 			            }
 
-			            for (int j=1;j<=i;j++){
-			                System.out.print(j+" ");
+			            for (int k=i;k<=n;k++){
+			                System.out.print(k+" ");
 			            }
 			            System.out.println();
 			        }*/
-			                /*
+			                /*  o/p 
 1 2 3 4 5 
- 1 2 3 4 
-  1 2 3 
-   1 2 
-    1 */
+  2 3 4 5 
+   3 4 5 
+    4 5 
+     5  */ 
 			        
 //qst5) java prg to print int,double and char on screen
 			     /*   Scanner sc=new Scanner(System.in);
